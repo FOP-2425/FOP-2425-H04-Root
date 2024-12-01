ddaa589e94bcb80e77e8e9b1bc3c1c27e77ef8cb
Daniel Mangold <mangold.daniel@protonmail.com>
2024-12-01 14:32:19 +0100

    Rewrite Rook and Bishop tests (H4.5.2)

--- src/graderPrivate/java/h04/chesspieces/BishopTest.java
+++ src/graderPrivate/java/h04/chesspieces/BishopTest.java
@@ -2,25 +2,24 @@ package h04.chesspieces;
 
 import fopbot.Robot;
 import fopbot.World;
-import kotlin.Triple;
+import h04.movement.DiagonalMover;
+import h04.movement.MoveStrategy;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
+import org.objectweb.asm.Type;
 import org.sourcegrade.jagr.api.rubric.TestForSubmission;
+import org.tudalgo.algoutils.transform.SubmissionExecutionHandler;
+import org.tudalgo.algoutils.transform.util.ClassHeader;
 import org.tudalgo.algoutils.tutor.general.assertions.Context;
 
 import java.awt.Point;
+import java.lang.reflect.Method;
 import java.util.Arrays;
-import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
 
-import static h04.Links.BISHOP_GET_POSSIBLE_MOVE_FIELDS_LINK;
-import static h04.Links.BISHOP_MOVE_STRATEGY_LINK;
-import static h04.Links.DIAGONAL_MOVER_GET_DIAGONAL_MOVES_LINK;
-import static h04.Links.DIAGONAL_MOVER_LINK;
-import static h04.Links.MOVE_STRATEGY_LINK;
-import static h04.Links.MOVE_STRATEGY_MOVE_LINK;
 import static org.tudalgo.algoutils.tutor.general.assertions.Assertions2.assertEquals;
-import static org.tudalgo.algoutils.tutor.general.assertions.Assertions2.assertNotNull;
 import static org.tudalgo.algoutils.tutor.general.assertions.Assertions2.assertSame;
 import static org.tudalgo.algoutils.tutor.general.assertions.Assertions2.assertTrue;
 import static org.tudalgo.algoutils.tutor.general.assertions.Assertions2.call;
@@ -31,65 +30,84 @@ import static org.tudalgo.algoutils.tutor.general.assertions.Assertions2.emptyCo
 @TestForSubmission
 public class BishopTest {
 
+    private final SubmissionExecutionHandler executionHandler = SubmissionExecutionHandler.getInstance();
+
+    private static Method moveStrategyMethod;
+    private static Method getPossibleMoveFieldsMethod;
+
+    @BeforeAll
+    public static void setup() {
+        try {
+            moveStrategyMethod = Bishop.class.getDeclaredMethod("moveStrategy", int.class, int.class, MoveStrategy.class);
+            getPossibleMoveFieldsMethod = Bishop.class.getDeclaredMethod("getPossibleMoveFields");
+        } catch (ReflectiveOperationException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @AfterEach
+    public void tearDown() {
+        executionHandler.resetMethodInvocationLogging();
+        executionHandler.resetMethodSubstitution();
+        executionHandler.resetMethodDelegation();
+    }
+
     @Test
     public void testClassHeader() {
-        List<Class<?>> bishopInterfaces = Arrays.asList(Bishop.class.getInterfaces());
-        assertTrue(bishopInterfaces.contains(DIAGONAL_MOVER_LINK.get().reflection()), emptyContext(), result ->
-            "Class Bishop does not implement interface DiagonalMover");
+        ClassHeader originalClassHeader = SubmissionExecutionHandler.getOriginalClassHeader(Bishop.class);
+        assertTrue(Arrays.stream(originalClassHeader.interfaces()).anyMatch(s -> s.equals(Type.getInternalName(DiagonalMover.class))),
+            emptyContext(),
+            result -> "Class h04.chesspieces.Bishop does not implement interface h04.movement.DiagonalMover");
     }
 
     @Test
     public void testMoveStrategy() {
+        executionHandler.disableMethodDelegation(moveStrategyMethod);
+
         World.setSize(3, 3);
         World.setDelay(0);
-        AtomicReference<Triple<Robot, Integer, Integer>> moveInvocation = new AtomicReference<>();
-        Object moveStrategyMock = Mockito.mock(MOVE_STRATEGY_LINK.get().reflection(), invocation -> {
-            if (invocation.getMethod().equals(MOVE_STRATEGY_MOVE_LINK.get().reflection())) {
-                moveInvocation.set(new Triple<>(invocation.getArgument(0), invocation.getArgument(1), invocation.getArgument(2)));
-            }
-            return Mockito.RETURNS_DEFAULTS.answer(invocation);
-        });
+        AtomicReference<Robot> robotRef = new AtomicReference<>();
+        AtomicInteger dxInt = new AtomicInteger(-1);
+        AtomicInteger dyInt = new AtomicInteger(-1);
+        MoveStrategy moveStrategy = (r, dx, dy) -> {
+            robotRef.set(r);
+            dxInt.set(dx);
+            dyInt.set(dy);
+        };
         int dx = 1;
         int dy = 1;
         Context context = contextBuilder()
             .add("dx", dx)
             .add("dy", dy)
-            .add("strategy", moveStrategyMock)
+            .add("strategy", moveStrategy)
             .build();
 
         Bishop bishopInstance = new Bishop(0, 0, Team.WHITE);
-        call(() -> BISHOP_MOVE_STRATEGY_LINK.get().invoke(bishopInstance, dx, dy, moveStrategyMock), context, result ->
+        call(() -> bishopInstance.moveStrategy(dx, dy, moveStrategy), context, result ->
             "An exception occurred while invoking moveStrategy(int, int, MoveStrategy)");
-        assertNotNull(moveInvocation.get(), context, result -> "Method move of MoveStrategy was not called");
-        assertSame(bishopInstance, moveInvocation.get().getFirst(), context, result ->
+        assertSame(bishopInstance, robotRef.get(), context, result ->
             "Method move of MoveStrategy was called with incorrect first parameter");
-        assertEquals(dx, moveInvocation.get().getSecond(), context, result ->
+        assertEquals(dx, dxInt.get(), context, result ->
             "Method move of MoveStrategy was called with incorrect second parameter");
-        assertEquals(dy, moveInvocation.get().getThird(), context, result ->
+        assertEquals(dy, dyInt.get(), context, result ->
             "Method move of MoveStrategy was called with incorrect third parameter");
     }
 
     @Test
-    public void testGetPossibleMoveFields() {
+    public void testGetPossibleMoveFields() throws ReflectiveOperationException {
+        executionHandler.disableMethodDelegation(getPossibleMoveFieldsMethod);
+        Point[] points = new Point[0];
+        executionHandler.substituteMethod(DiagonalMover.class.getDeclaredMethod("getDiagonalMoves"), invocation -> points);
+
         World.setSize(3, 3);
         World.setDelay(0);
-        Point[] points = new Point[0];
-        Bishop bishopMock = Mockito.mock(Bishop.class, Mockito.withSettings()
-            .useConstructor(0, 0, Team.WHITE)
-            .defaultAnswer(invocation -> {
-                if (invocation.getMethod().equals(DIAGONAL_MOVER_GET_DIAGONAL_MOVES_LINK.get().reflection())) {
-                    return points;
-                } else {
-                    return invocation.callRealMethod();
-                }
-            }));
         Context context = contextBuilder()
             .add("world size", 3)
             .add("x", 0)
             .add("y", 0)
             .build();
 
-        Point[] returnValue = callObject(() -> BISHOP_GET_POSSIBLE_MOVE_FIELDS_LINK.get().invoke(bishopMock), context, result ->
+        Point[] returnValue = callObject(() -> new Bishop(0, 0, Team.WHITE).getPossibleMoveFields(), context, result ->
             "An exception occurred while invoking getPossibleMoveFields()");
         assertSame(points, returnValue, context, result -> "Method did not return the result of getPossibleMoveFields()");
     }
--- src/graderPrivate/java/h04/chesspieces/RookTest.java
+++ src/graderPrivate/java/h04/chesspieces/RookTest.java
@@ -2,25 +2,24 @@ package h04.chesspieces;
 
 import fopbot.Robot;
 import fopbot.World;
-import kotlin.Triple;
+import h04.movement.MoveStrategy;
+import h04.movement.OrthogonalMover;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
+import org.objectweb.asm.Type;
 import org.sourcegrade.jagr.api.rubric.TestForSubmission;
+import org.tudalgo.algoutils.transform.SubmissionExecutionHandler;
+import org.tudalgo.algoutils.transform.util.ClassHeader;
 import org.tudalgo.algoutils.tutor.general.assertions.Context;
 
 import java.awt.Point;
+import java.lang.reflect.Method;
 import java.util.Arrays;
-import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
 
-import static h04.Links.MOVE_STRATEGY_LINK;
-import static h04.Links.MOVE_STRATEGY_MOVE_LINK;
-import static h04.Links.ORTHOGONAL_MOVER_GET_ORTHOGONAL_MOVES_LINK;
-import static h04.Links.ORTHOGONAL_MOVER_LINK;
-import static h04.Links.ROOK_GET_POSSIBLE_MOVE_FIELDS_LINK;
-import static h04.Links.ROOK_MOVE_STRATEGY_LINK;
 import static org.tudalgo.algoutils.tutor.general.assertions.Assertions2.assertEquals;
-import static org.tudalgo.algoutils.tutor.general.assertions.Assertions2.assertNotNull;
 import static org.tudalgo.algoutils.tutor.general.assertions.Assertions2.assertSame;
 import static org.tudalgo.algoutils.tutor.general.assertions.Assertions2.assertTrue;
 import static org.tudalgo.algoutils.tutor.general.assertions.Assertions2.call;
@@ -31,66 +30,86 @@ import static org.tudalgo.algoutils.tutor.general.assertions.Assertions2.emptyCo
 @TestForSubmission
 public class RookTest {
 
+    private final SubmissionExecutionHandler executionHandler = SubmissionExecutionHandler.getInstance();
+
+    private static Method moveStrategyMethod;
+    private static Method getPossibleMoveFieldsMethod;
+
+    @BeforeAll
+    public static void setup() {
+        try {
+            moveStrategyMethod = Rook.class.getDeclaredMethod("moveStrategy", int.class, int.class, MoveStrategy.class);
+            getPossibleMoveFieldsMethod = Rook.class.getDeclaredMethod("getPossibleMoveFields");
+        } catch (ReflectiveOperationException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @AfterEach
+    public void tearDown() {
+        executionHandler.resetMethodInvocationLogging();
+        executionHandler.resetMethodSubstitution();
+        executionHandler.resetMethodDelegation();
+    }
+
     @Test
     public void testClassHeader() {
-        List<Class<?>> rookInterfaces = Arrays.asList(Rook.class.getInterfaces());
-        assertTrue(rookInterfaces.contains(ORTHOGONAL_MOVER_LINK.get().reflection()), emptyContext(), result ->
-            "Class Rook does not implement interface OrthogonalMover");
+        ClassHeader originalClassHeader = SubmissionExecutionHandler.getOriginalClassHeader(Rook.class);
+        assertTrue(Arrays.stream(originalClassHeader.interfaces()).anyMatch(s -> s.equals(Type.getInternalName(OrthogonalMover.class))),
+            emptyContext(),
+            result -> "Class h04.chesspieces.Rook does not implement interface h04.movement.OrthogonalMover");
     }
 
     @Test
     public void testMoveStrategy() {
+        executionHandler.disableMethodDelegation(moveStrategyMethod);
+
         World.setSize(3, 3);
         World.setDelay(0);
-        AtomicReference<Triple<Robot, Integer, Integer>> moveInvocation = new AtomicReference<>();
-        Object moveStrategyMock = Mockito.mock(MOVE_STRATEGY_LINK.get().reflection(), invocation -> {
-            if (invocation.getMethod().equals(MOVE_STRATEGY_MOVE_LINK.get().reflection())) {
-                moveInvocation.set(new Triple<>(invocation.getArgument(0), invocation.getArgument(1), invocation.getArgument(2)));
-            }
-            return Mockito.RETURNS_DEFAULTS.answer(invocation);
-        });
+        AtomicReference<Robot> robotRef = new AtomicReference<>();
+        AtomicInteger dxInt = new AtomicInteger(-1);
+        AtomicInteger dyInt = new AtomicInteger(-1);
+        MoveStrategy moveStrategy = (r, dx, dy) -> {
+            robotRef.set(r);
+            dxInt.set(dx);
+            dyInt.set(dy);
+        };
         int dx = 1;
         int dy = 1;
         Context context = contextBuilder()
             .add("dx", dx)
             .add("dy", dy)
-            .add("strategy", moveStrategyMock)
+            .add("strategy", moveStrategy)
             .build();
 
         Rook rookInstance = new Rook(0, 0, Team.WHITE);
-        call(() -> ROOK_MOVE_STRATEGY_LINK.get().invoke(rookInstance, dx, dy, moveStrategyMock), context, result ->
+        call(() -> rookInstance.moveStrategy(dx, dy, moveStrategy), context, result ->
             "An exception occurred while invoking moveStrategy(int, int, MoveStrategy)");
-        assertNotNull(moveInvocation.get(), context, result -> "Method move of MoveStrategy was not called");
-        assertSame(rookInstance, moveInvocation.get().getFirst(), context, result ->
+        assertSame(rookInstance, robotRef.get(), context, result ->
             "Method move of MoveStrategy was called with incorrect first parameter");
-        assertEquals(dx, moveInvocation.get().getSecond(), context, result ->
+        assertEquals(dx, dxInt.get(), context, result ->
             "Method move of MoveStrategy was called with incorrect second parameter");
-        assertEquals(dy, moveInvocation.get().getThird(), context, result ->
+        assertEquals(dy, dyInt.get(), context, result ->
             "Method move of MoveStrategy was called with incorrect third parameter");
     }
 
     @Test
-    public void testGetPossibleMoveFields() {
-        World.setSize(3, 3);
-        World.setDelay(0);
+    public void testGetPossibleMoveFields() throws ReflectiveOperationException {
+        executionHandler.disableMethodDelegation(getPossibleMoveFieldsMethod);
         Point[] points = new Point[0];
-        Rook rookMock = Mockito.mock(Rook.class, Mockito.withSettings()
-            .useConstructor(0, 0, Team.WHITE)
-            .defaultAnswer(invocation -> {
-                if (invocation.getMethod().equals(ORTHOGONAL_MOVER_GET_ORTHOGONAL_MOVES_LINK.get().reflection())) {
-                    return points;
-                } else {
-                    return invocation.callRealMethod();
-                }
-            }));
+        executionHandler.substituteMethod(OrthogonalMover.class.getDeclaredMethod("getOrthogonalMoves"), invocation -> points);
+
+        int worldSize = 3;
+        World.setSize(worldSize, worldSize);
+        World.setDelay(0);
         Context context = contextBuilder()
-            .add("world size", 3)
+            .add("world size", worldSize)
             .add("x", 0)
             .add("y", 0)
             .build();
 
-        Point[] returnValue = callObject(() -> ROOK_GET_POSSIBLE_MOVE_FIELDS_LINK.get().invoke(rookMock), context, result ->
+        Point[] returnValue = callObject(() -> new Rook(0, 0, Team.WHITE).getPossibleMoveFields(), context, result ->
             "An exception occurred while invoking getPossibleMoveFields()");
-        assertSame(points, returnValue, context, result -> "Method did not return the result of getPossibleMoveFields()");
+        assertSame(points, returnValue, context, result -> "Method did not return the result of getOrthogonalMoves()");
     }
 }
