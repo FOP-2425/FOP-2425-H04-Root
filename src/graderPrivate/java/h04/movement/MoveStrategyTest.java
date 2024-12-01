569be60d29e4ad6c27a79ea001c945934d16d9fa
Daniel Mangold <mangold.daniel@protonmail.com>
2024-12-01 12:45:22 +0100

    Rewrite MoveStrategy tests (H4.2.1)

--- src/graderPrivate/java/h04/movement/MoveStrategyTest.java
+++ src/graderPrivate/java/h04/movement/MoveStrategyTest.java
@@ -1,21 +1,31 @@
 package h04.movement;
 
-import h04.Links;
+import fopbot.Robot;
 import org.junit.jupiter.api.Test;
+import org.objectweb.asm.Type;
 import org.sourcegrade.jagr.api.rubric.TestForSubmission;
-import org.tudalgo.algoutils.tutor.general.reflections.MethodLink;
-import org.tudalgo.algoutils.tutor.general.reflections.TypeLink;
+import org.tudalgo.algoutils.transform.SubmissionExecutionHandler;
+import org.tudalgo.algoutils.transform.util.ClassHeader;
+import org.tudalgo.algoutils.transform.util.MethodHeader;
 
-import static org.tudalgo.algoutils.tutor.general.assertions.Assertions2.assertEquals;
-import static org.tudalgo.algoutils.tutor.general.assertions.Assertions2.emptyContext;
+import java.lang.reflect.Modifier;
+
+import static org.tudalgo.algoutils.tutor.general.assertions.Assertions2.*;
 
 @TestForSubmission
 public class MoveStrategyTest {
 
     @Test
     public void testDeclaration() {
-        TypeLink moveStrategyLink = Links.MOVE_STRATEGY_LINK.get();  // implicit test that interface exists
-        MethodLink moveLink = Links.MOVE_STRATEGY_MOVE_LINK.get();  // same here + parameters
-        assertEquals(void.class, moveLink.returnType().reflection(), emptyContext(), result -> "Return type of method move is not void");
+        ClassHeader originalClassHeader = SubmissionExecutionHandler.getOriginalClassHeader(MoveStrategy.class);
+        assertTrue(Modifier.isInterface(originalClassHeader.access()), emptyContext(), result ->
+            "Class h04.movement.MoveStrategy is not an interface");
+
+        MethodHeader moveMethodHeader = SubmissionExecutionHandler.getOriginalMethodHeaders(MoveStrategy.class)
+            .stream()
+            .filter(methodHeader -> methodHeader.name().equals("move") &&
+                methodHeader.descriptor().equals(Type.getMethodDescriptor(Type.VOID_TYPE, Type.getType(Robot.class), Type.INT_TYPE, Type.INT_TYPE)))
+            .findAny()
+            .orElseGet(() -> fail(emptyContext(), result -> "MoveStrategy does not declare method 'move(Robot, int, int)'"));
     }
 }
